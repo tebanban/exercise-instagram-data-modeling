379ada5098f50de42880c6792160f5af6fad0b5b
tebanban <estebancalf@gmail.com>
2021-04-14 04:51:50 +0000

    updated

--- src/models.py
+++ src/models.py
@@ -9,27 +9,45 @@ from eralchemy import render_er
 Base = declarative_base()
 
 class User(Base):
-    __tablename__ = 'User'
+    __tablename__ = 'user'
+    # Here we define columns for the table person
+    # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    Nickname = Column(String(15))
-    Correo = Column(String(30))
-    Telefono = Column(Integer)
-    Sexo = Column(Integer)
-    Estado_Cuenta = Column(Integer, ForeignKey('Login.Estado_Cuenta'))
-    Nombre_Usuario = Column(Integer, ForeignKey('Login.Nombre_Usuario'))
-    Primer_Apellido = Column(Integer, ForeignKey('Login.Primer_Apellido'))
-    Segundo_Apellido = Column(Integer, ForeignKey('Login.Segundo_Apellido'))
-    Password = Column(Integer, ForeignKey('Login.Password'))
-    Confirmacion_Password = Column(Integer, ForeignKey('Login.Confirmacion_Password'))
+    name = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
+    password = Column(String(250), nullable=False)
+    created_at = Column(String(250))
+
+class Posts(Base):
+    __tablename__ = 'posts'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    post_type = Column(String(250))
+    post_comment = Column(String(250))
+    created = Column(String(250))
+    
+class Posts_comments(Base):
+    __tablename__ = 'comments'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    post_id = Column(Integer, ForeignKey('posts.id'))
+    comment = Column(String(250))
+    created = Column(String(250))
     
 
-class Like(Base):
-    __tablename__ = 'like'
+class Likes(Base):
+    __tablename__ = 'likes'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-
-class Comment(Base):
-    __tablename__ = 'comment'
-    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    post_id = Column(Integer, ForeignKey('posts.id'))
+    created = Column(String(250))
+    
     
 
     def to_dict(self):
